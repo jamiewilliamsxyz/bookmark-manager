b9b2d631d39b94d1eee1632e5b7e2038ebffd2a3
Jamie <jamie7williams@gmail.com>
2025-11-06 14:27:00 +0000

    Refactor home page styles & elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 const Home = () => {
   return (
-    <main className="flex min-h-screen justify-center p-14">
-      <h1 className="text-6xl font-semibold">
-        <span className="text-blue-400">Bookmark</span> Manager
-      </h1>
-    </main>
+    <div className="flex min-h-screen justify-start items-center p-14 flex-col gap-14">
+      <header className="flex flex-col items-center text-center gap-6">
+        <h1 className="text-7xl font-semibold">
+          <span className="text-blue-400">Bookmark</span> Manager
+        </h1>
+        <h2 className="font-medium text-xl max-w-100">
+          A simple bookmark manager to keep track of important links
+        </h2>
+      </header>
+    </div>
   );
 };
 
