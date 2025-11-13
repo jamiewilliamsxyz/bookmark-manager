5dd25930fa2baebceec2ce8b2b6c9b0767cbecaf
Jamie <jamie7williams@gmail.com>
2025-11-13 16:43:05 +0000

    Refactor styles; add auth redirect link

--- src/components/features/auth/AuthForm.tsx
+++ src/components/features/auth/AuthForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useActionState } from "react";
+import Link from "next/link";
 import { useAuth } from "@/hooks/useAuth";
 import type { AuthFormProps, AuthResult } from "@/types";
 
@@ -45,15 +46,15 @@ const AuthForm = ({ type }: AuthFormProps) => {
   return (
     <form
       action={authFormAction}
-      className="flex flex-col gap-5 w-full max-w-sm px-5 py-8 bg-neutral-100 shadow-lg rounded-lg text-neutral-700"
+      className="flex flex-col gap-6 w-full max-w-sm px-5 py-6 bg-neutral-100 shadow-lg rounded-2xl text-neutral-700"
     >
-      <h1 className="text-neutral-800 text-4xl font-semibold text-center leading-none">
+      <h1 className="text-neutral-800 text-4xl font-semibold text-center">
         {type === "login" ? "Log In" : "Sign Up"}
       </h1>
 
       {confirmation.isConfirming && type === "signup" ? (
         <>
-          <p className="mt-1 text-center text-[1.05rem] text-neutral-600">
+          <p className="text-center text-[1.05rem] text-neutral-600">
             {confirmation.message}
           </p>
           <button
@@ -72,7 +73,7 @@ const AuthForm = ({ type }: AuthFormProps) => {
               name="email"
               type="email"
               placeholder="Enter your email"
-              className="border p-2 rounded-lg w-full mt-2"
+              className="border p-2 rounded-lg w-full mt-1.5"
               required
               aria-required="true"
             />
@@ -85,7 +86,7 @@ const AuthForm = ({ type }: AuthFormProps) => {
               name="password"
               type="password"
               placeholder="Enter your password"
-              className="border p-2 rounded-lg w-full mt-2"
+              className="border p-2 rounded-lg w-full mt-1.5"
               aria-required="true"
               required
             />
@@ -94,10 +95,34 @@ const AuthForm = ({ type }: AuthFormProps) => {
           <button
             type="submit"
             disabled={pending}
-            className="mt-3.5 cursor-pointer p-2 font-medium text-lg w-full rounded-lg shadow-lg bg-neutral-800 text-neutral-100 hover:opacity-75 duration-200 transition-opacity"
+            className="mt-2.5 cursor-pointer p-2 font-medium text-lg w-full rounded-lg shadow-lg bg-neutral-800 text-neutral-100 hover:opacity-75 duration-200 transition-opacity"
           >
             {pending ? "Loading..." : type === "login" ? "Log In" : "Sign Up"}
           </button>
+
+          <div className="flex gap-1.5 text-[0.95rem] text-neutral-600">
+            {type === "login" ? (
+              <>
+                <p>Don&apos;t have an account?</p>
+                <Link
+                  href="/signup"
+                  className="underline text-neutral-800 hover:opacity-75 cursor-pointer duration-200 transition-opacity"
+                >
+                  Sign up
+                </Link>
+              </>
+            ) : (
+              <>
+                <p>Already have an account?</p>
+                <Link
+                  href="/login"
+                  className="underline text-neutral-800 hover:opacity-75 cursor-pointer duration-200 transition-opacity"
+                >
+                  Log in
+                </Link>
+              </>
+            )}
+          </div>
         </>
       )}
     </form>
