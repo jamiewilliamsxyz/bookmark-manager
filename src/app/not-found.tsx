b1cfc996222dc3d0c546b2958f8b46871a7288d6
Jamie <jamie7williams@gmail.com>
2025-11-16 21:20:19 +0000

    Refactor styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,8 +8,8 @@ const NotFoundPage = () => {
 
   return (
     <div className="flex flex-1 flex-col justify-center items-center px-8 w-full gap-4">
-      <h1 className="text-8xl font-semibold text-neutral-100">404</h1>
-      <h2 className="text-xl">Page not found...</h2>
+      <h1 className="text-6xl">404</h1>
+      <h2 className="text-lg">Page not found</h2>
       <Link
         href={session ? "/bookmarks" : "/"}
         className="underline text-lg hover:opacity-75 duration-200 transition-opacity cursor-pointer"
