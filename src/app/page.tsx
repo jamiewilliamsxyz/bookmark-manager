d642cfda3a2e2ac5893370d77595d213097f879b
Jamie <jamie7williams@gmail.com>
2025-11-18 13:35:40 +0000

    Refactor landing page
    Implemented new landing page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,30 @@
+import Link from "next/link";
+
 const Home = () => {
   return (
-    <div className="flex justify-start items-center p-14 flex-col gap-14">
-      <header className="flex flex-col items-center text-center gap-6">
-        <h1 className="text-7xl font-semibold text-neutral-100">
-          <span className="text-sky-300">Bookmark</span> Manager
-        </h1>
-        <h2 className="font-medium text-xl max-w-100">
-          A simple bookmark manager to keep track of important links
-        </h2>
-      </header>
-      {/*
-        Video of app
-        CTA
-      */}
+    <div className="flex-1 flex justify-center">
+      <div className="w-[800px] flex flex-col justify-center items-center gap-12">
+        <header className="flex gap-6 flex-col items-center text-center">
+          <h1 className="text-6xl">
+            Save, tag and search your links all in one place
+          </h1>
+          <h2 className="text-lg max-w-1/2">
+            Stop losing important links. Save, tag and search them in a
+            lightweight, streamlined interface.
+          </h2>
+        </header>
+
+        <div className="flex gap-9.5 flex-col items-center">
+          <div className="bg-neutral-200 rounded-md shadow h-[360px] w-[640px] text-neutral-800">
+            placeholder video
+          </div>
+          <Link href="/signup">
+            <button className="bg-neutral-100 px-5.5 py-2 text-lg rounded-md text-neutral-800 cursor-pointer hover:opacity-75 duration-200 transition-opacity">
+              Start organising
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
