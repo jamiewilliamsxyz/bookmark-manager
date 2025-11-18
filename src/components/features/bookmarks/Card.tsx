b4ddee76dcc4b24b9831fd072d9605bf82dff3b2
Jamie <jamie7williams@gmail.com>
2025-11-18 11:59:04 +0000

    Ajust styles
    Change card icons colours; ajust card border radius

--- src/components/features/bookmarks/Card.tsx
+++ src/components/features/bookmarks/Card.tsx
@@ -27,7 +27,7 @@ const Card = () => {
   }, []);
 
   return (
-    <div className="bg-[#1a1a1a] border border-neutral-800 shadow rounded-xl p-5 w-96 max-w-96 flex flex-col gap-5">
+    <div className="bg-[#1a1a1a] border border-neutral-800 shadow rounded-md p-5 w-96 max-w-96 flex flex-col gap-5">
       <div className="flex items-center justify-between">
         <p className="text-lg leading-0">GitHub profile</p>
         <div className="flex gap-4">
@@ -43,12 +43,12 @@ const Card = () => {
             {isCooldown ? (
               <p className="text-sm">Copied!</p>
             ) : (
-              <Clipboard color="#e5e5e5" strokeWidth={1} size={20} />
+              <Clipboard color="#f5f5f5" strokeWidth={1} size={20} />
             )}
           </button>
 
           <button className="cursor-pointer hover:opacity-75 duration-200 transition-opacity">
-            <SquarePen color="#e5e5e5" strokeWidth={1} size={20} />
+            <SquarePen color="#f5f5f5" strokeWidth={1} size={20} />
           </button>
 
           <button className="cursor-pointer hover:opacity-75 duration-200 transition-opacity">
