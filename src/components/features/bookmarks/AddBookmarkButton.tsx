3a39ebe9c682c832b0cc36f58cdfa93daae93442
Jamie <jamie7williams@gmail.com>
2025-11-21 14:21:51 +0000

    Implement bookmark limit

--- src/components/features/bookmarks/AddBookmarkButton.tsx
+++ src/components/features/bookmarks/AddBookmarkButton.tsx
@@ -1,14 +1,22 @@
 "use client";
 
+import { useBookmarks } from "@/hooks/useBookmarks";
 import { useModal } from "@/hooks/useModal";
+import { MAX_BOOKMARKS } from "@/constants/bookmarks";
 
 const AddBookmarkButton = () => {
   const { openModal } = useModal();
+  const { bookmarks } = useBookmarks();
+
+  const openBookmarkForm = () => {
+    if (bookmarks.length >= MAX_BOOKMARKS) return;
+    openModal("bookmarkForm");
+  };
 
   return (
     <button
-      onClick={() => openModal("bookmarkForm")}
-      className="bg-neutral-100 py-2 px-5.5 w-fit rounded-md shadow text-neutral-800 cursor-pointer hover:opacity-75 duration-200 transition-opacity"
+      onClick={openBookmarkForm}
+      className="text-neutral-800 bg-neutral-100 py-2 px-5.5 w-fit rounded-md shadow hover:opacity-75 cursor-pointer duration-200 transition-opacity"
     >
       Add bookmark
     </button>
--- src/components/features/bookmarks/CardsContainer.tsx
+++ src/components/features/bookmarks/CardsContainer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useBookmarks } from "@/hooks/useBookmarks";
+import { MAX_BOOKMARKS } from "@/constants/bookmarks";
 import Card from "./Card";
 
 const CardsContainer = () => {
@@ -11,21 +12,26 @@ const CardsContainer = () => {
   }
 
   return (
-    <div className="flex flex-wrap gap-5 w-1/2 items-center justify-center">
-      {bookmarks.length > 0 ? (
-        bookmarks.map((b) => (
-          <Card
-            key={b.id}
-            id={b.id}
-            title={b.title}
-            url={b.url}
-            tags={b.tags}
-          />
-        ))
-      ) : (
-        <p>You currently don&apos;t have any bookmarks</p>
+    <>
+      {bookmarks.length >= MAX_BOOKMARKS && (
+        <p className="text-red-500">{`Maximum of ${MAX_BOOKMARKS} bookmarks reached`}</p>
       )}
-    </div>
+      <div className="flex flex-wrap gap-5 w-1/2 items-center justify-center">
+        {bookmarks.length > 0 ? (
+          bookmarks.map((b) => (
+            <Card
+              key={b.id}
+              id={b.id}
+              title={b.title}
+              url={b.url}
+              tags={b.tags}
+            />
+          ))
+        ) : (
+          <p>You currently don&apos;t have any bookmarks</p>
+        )}
+      </div>
+    </>
   );
 };
 
