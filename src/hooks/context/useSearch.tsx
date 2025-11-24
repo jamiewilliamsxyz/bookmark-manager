f278ab8bd6d1fd1c58f0f472500a23adedbecc83
Jamie <jamie7williams@gmail.com>
2025-11-24 13:06:24 +0000

    Edit error message

--- src/hooks/context/useSearch.tsx
+++ src/hooks/context/useSearch.tsx
@@ -4,7 +4,7 @@ import { SearchContext } from "@/context/SearchContext";
 export const useSearch = () => {
   const context = use(SearchContext);
   if (!context) {
-    throw new Error("useSearch needs to be used in useSearch");
+    throw new Error("useSearch needs to be used in SearchProvider");
   }
   return context;
 };
