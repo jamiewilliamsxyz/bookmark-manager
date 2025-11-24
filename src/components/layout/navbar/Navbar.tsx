050d948db288768d72c5cd27d059eaeab38167cb
Jamie <jamie7williams@gmail.com>
2025-11-24 15:11:13 +0000

    Ajust navbar padding

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import NavItems from "./NavItems";
 const Navbar = () => {
   return (
     <nav className="border-b border-b-neutral-800">
-      <div className="w-[800px] mx-auto flex justify-between py-6 items-center">
+      <div className="w-[800px] mx-auto flex justify-between py-5 items-center">
         <NavLogo />
         <ul className="flex gap-11">
           <NavItems />
