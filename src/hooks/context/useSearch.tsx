bcc4bea907ed1318bd5348dd05263340bc625893
Jamie <jamie7williams@gmail.com>
2025-11-24 13:04:46 +0000

    Create useSearch hook to consume search context

---
+++ src/hooks/context/useSearch.tsx
@@ -0,0 +1,10 @@
+import { use } from "react";
+import { SearchContext } from "@/context/SearchContext";
+
+export const useSearch = () => {
+  const context = use(SearchContext);
+  if (!context) {
+    throw new Error("useSearch needs to be used in useSearch");
+  }
+  return context;
+};
