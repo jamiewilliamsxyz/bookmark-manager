8f2ad8ea89e5b79147be7607d72030379cf87533
Jamie <jamie7williams@gmail.com>
2025-11-25 16:36:59 +0000

    Ajust filter/placeholder styles

--- src/components/features/bookmarks/bookmark-filter/FilterTrigger.tsx
+++ src/components/features/bookmarks/bookmark-filter/FilterTrigger.tsx
@@ -10,10 +10,10 @@ const FilterTrigger = ({ selected, onToggle }: FilterTriggerProps) => {
   return (
     <div
       onClick={onToggle}
-      className="text-neutral-400 shadow py-2 pr-3 pl-3.5 cursor-pointer bg-[#1a1a1a] rounded-md border border-neutral-800 flex items-center justify-between w-43"
+      className="text-neutral-400 shadow py-2 pr-3 pl-3.5 cursor-pointer bg-[#1a1a1a] rounded-md border border-neutral-800 flex items-center justify-between w-44"
     >
       <div className="flex items-center">
-        <p className="mr-1.5">Search by</p>
+        <p className="mr-1.5">Search by:</p>
         <p className="text-neutral-200">{selected}</p>
       </div>
 
