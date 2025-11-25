f45e186a7d06cc64642d3f35f767072330e3284a
Jamie <jamie7williams@gmail.com>
2025-11-25 17:00:54 +0000

    Adjust gap

--- src/components/features/auth/EmailBox.tsx
+++ src/components/features/auth/EmailBox.tsx
@@ -6,7 +6,7 @@ const EmailBox = () => {
   const { session } = useAuth();
 
   return (
-    <div className="bg-[#1a1a1a] border border-neutral-800 shadow rounded-md p-5 flex gap-2 items-center justify-center select-none w-full">
+    <div className="bg-[#1a1a1a] border border-neutral-800 shadow rounded-md p-5 flex gap-1.75 items-center justify-center select-none w-full">
       <p>Logged in as:</p>
 
       <p className="text-neutral-400">{session?.user.email}</p>
