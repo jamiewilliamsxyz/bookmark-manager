3b79a34b56f317693d335b1acc8f57783b020497
Jamie <jamie7williams@gmail.com>
2025-11-25 16:59:20 +0000

    Ajust styles

--- src/components/features/auth/EmailBox.tsx
+++ src/components/features/auth/EmailBox.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import { ArrowRight } from "lucide-react";
 import { useAuth } from "@/hooks/context/useAuth";
 
 const EmailBox = () => {
   const { session } = useAuth();
 
   return (
-    <div className="bg-[#1a1a1a] border border-neutral-800 shadow rounded-md p-5 flex gap-3 items-center justify-center select-none">
-      <p>Logged in as</p>
-      <ArrowRight color="#e5e5e5" strokeWidth={1.5} size={18} />
+    <div className="bg-[#1a1a1a] border border-neutral-800 shadow rounded-md p-5 flex gap-2 items-center justify-center select-none w-full">
+      <p>Logged in as:</p>
+
       <p className="text-neutral-400">{session?.user.email}</p>
     </div>
   );
