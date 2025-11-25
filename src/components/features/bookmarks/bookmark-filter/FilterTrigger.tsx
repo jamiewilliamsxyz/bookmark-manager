c913feba5a0ae0be2d3584989911224c23ffa327
Jamie <jamie7williams@gmail.com>
2025-11-25 16:19:33 +0000

    Ajust selected filter text colour

--- src/components/features/bookmarks/bookmark-filter/FilterTrigger.tsx
+++ src/components/features/bookmarks/bookmark-filter/FilterTrigger.tsx
@@ -12,9 +12,9 @@ const FilterTrigger = ({ selected, onToggle }: FilterTriggerProps) => {
       onClick={onToggle}
       className="text-neutral-400 shadow py-2 pr-3 pl-3.5 cursor-pointer bg-[#1a1a1a] rounded-md border border-neutral-800 gap-2.25 flex items-center"
     >
-      <div className="flex items-center">
+      <div className="flex items-center w-">
         <p className="mr-1.5">Search by</p>
-        <p className="underline">{selected}</p>
+        <p className="text-neutral-200">{selected}</p>
       </div>
 
       <ChevronDown color="#f5f5f5" strokeWidth={1} size={20} />
