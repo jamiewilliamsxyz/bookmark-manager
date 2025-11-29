8a15273e8029b86834a551cf4c002d7f9fdc3061
Jamie <jamie7williams@gmail.com>
2025-11-29 14:06:59 +0000

    Move floating icons into its own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,11 @@
 import Link from "next/link";
-import { MoveRight, Bookmark, Link2, Tag } from "lucide-react";
+import { MoveRight } from "lucide-react";
+import FloatingIcons from "@/components/FloatingIcons";
 
 const Home = () => {
   return (
     <div className="flex-1 flex justify-center">
-      <div
-        aria-hidden="true"
-        className="absolute top-[45%] left-[calc(50%-410px)] rotate-12"
-      >
-        <Bookmark className="w-12 h-12 text-neutral-100 opacity-65 animate-[float_5s_ease-in-out_infinite]" />
-      </div>
-
-      <div
-        aria-hidden="true"
-        className="absolute top-1/2 right-[calc(50%-400px)] -rotate-6"
-      >
-        <Tag className="w-10 h-10 text-neutral-100 opacity-60 animate-[float_6s_ease-in-out_infinite]" />
-      </div>
-
-      <div
-        aria-hidden="true"
-        className="absolute top-2/3 left-[calc(50%-390px)] rotate-[-20deg]"
-      >
-        <Link2 className="w-8 h-8 text-neutral-100 opacity-50 animate-[float_5.5s_ease-in-out_infinite]" />
-      </div>
-
-      <div
-        aria-hidden="true"
-        className="absolute top-[70%] right-[calc(50%-400px)] -rotate-18"
-      >
-        <Bookmark className="w-10 h-10 text-neutral-100 opacity-55 animate-[float_6.5s_ease-in-out_infinite]" />
-      </div>
+      <FloatingIcons />
 
       <div className="w-[800px] flex flex-col justify-center items-center gap-12">
         <header className="flex gap-6 flex-col items-center text-center">
---
+++ src/components/FloatingIcons.tsx
@@ -0,0 +1,25 @@
+import { Bookmark, Link2, Tag } from "lucide-react";
+
+const FloatingIcons = () => {
+  return (
+    <div aria-hidden="true">
+      <div className="absolute top-[45%] left-[calc(50%-410px)] rotate-12">
+        <Bookmark className="w-12 h-12 text-neutral-100 opacity-65 animate-[float_5s_ease-in-out_infinite]" />
+      </div>
+
+      <div className="absolute top-1/2 right-[calc(50%-400px)] -rotate-6">
+        <Tag className="w-10 h-10 text-neutral-100 opacity-60 animate-[float_6s_ease-in-out_infinite]" />
+      </div>
+
+      <div className="absolute top-2/3 left-[calc(50%-390px)] rotate-[-20deg]">
+        <Link2 className="w-8 h-8 text-neutral-100 opacity-50 animate-[float_5.5s_ease-in-out_infinite]" />
+      </div>
+
+      <div className="absolute top-[70%] right-[calc(50%-400px)] -rotate-18">
+        <Bookmark className="w-10 h-10 text-neutral-100 opacity-55 animate-[float_6.5s_ease-in-out_infinite]" />
+      </div>
+    </div>
+  );
+};
+
+export default FloatingIcons;
