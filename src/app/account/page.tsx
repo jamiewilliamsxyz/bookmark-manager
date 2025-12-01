73ddc7109d45e12aae307c7e47b58c26d23503d9
Jamie <jamie7williams@gmail.com>
2025-12-01 16:29:57 +0000

    Center page content

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -3,7 +3,7 @@ import AccountActions from "@/components/features/auth/AccountActions";
 
 const AccountPage = () => {
   return (
-    <div className="flex flex-1 flex-col justify-start items-center py-12 gap-12">
+    <div className="flex flex-1 flex-col justify-center items-center py-12 gap-12">
       <h1 className="text-6xl">Account</h1>
       <div className="flex flex-col gap-5 items-center">
         <AccountActions />
