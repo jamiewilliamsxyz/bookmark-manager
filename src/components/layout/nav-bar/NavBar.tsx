b0e00d244980215f2d2ae5f5c29f43f790c6b915
Jamie <jamie7williams@gmail.com>
2025-12-01 19:08:30 +0000

    Adjust styles for mobile

--- src/components/layout/nav-bar/NavBar.tsx
+++ src/components/layout/nav-bar/NavBar.tsx
@@ -3,10 +3,10 @@ import NavItems from "./NavItems";
 
 const NavBar = () => {
   return (
-    <nav className="border-b border-b-neutral-800">
-      <div className="w-[800px] mx-auto flex justify-between py-4.5 items-center">
+    <nav className="border-b border-b-neutral-800 flex items-center justify-center">
+      <div className="flex justify-between items-center py-4.5 px-4 lg:px-0 max-w-[800px] w-full">
         <NavLogo />
-        <ul className="flex gap-11">
+        <ul className="flex gap-6 sm:gap-11">
           <NavItems />
         </ul>
       </div>
